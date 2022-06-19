833cf8bace47ccaca2b963fa2d6cffcf6062f242
mar1n <30738216+mar1n@users.noreply.github.com>
2022-06-19 20:34:08 +0100

    fetch data by axios and show img

--- src/component/Cats.js
+++ src/component/Cats.js
@@ -1,15 +1,29 @@
-import React, { useState, useEffect} from "react";
-import axios from "axios"
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Cats = () => {
-    useEffect(() => {
-        console.log('useeffect is in this componenet')
-        async function fetchCats() {
-            const axioResult = await axios.get("https://api.thecatapi.com/v1/breeds/search?q=sib");
-            console.log('axios', axioResult);
+  const [cat, setCat] = useState({
+    url: "",
+  });
+  useEffect(() => {
+    console.log("useeffect is in this componenet");
+    async function fetchCats() {
+      const { data: ourResult } = await axios.get(
+        "https://api.thecatapi.com/v1/images/search",
+        {
+          headers: {
+            "x-api-key": "972da10d-ef46-4937-9b57-6aca9a67fae0",
+          },
         }
-        fetchCats();
-    })
-}
+      );
+      const [result] = ourResult;
+      const {url} = result;
+      setCat({ url });
+    }
+    fetchCats();
+  }, []);
+  const {url } = cat;
+  return <div>{url ? <img src={url} alt="" /> : 'Loading...'}</div>;
+};
 
 export default Cats;
